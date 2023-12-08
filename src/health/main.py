3d4a4f1c38c5fcd87fae09238387a2cfcb6fa6d8
Goetz <nils.goetz@sap.com'git config --global user.email nils.goetz@sap.comgit config --global user.email "nils.goetz@sap.com>
2023-12-08 13:10:21 +0100

    adds entry point and source dir

---
+++ src/health/main.py
@@ -0,0 +1,14 @@
+import functions_framework
+
+@functions_framework.http
+def get_health(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    return 'Hello World!'
