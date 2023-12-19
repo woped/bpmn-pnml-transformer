d9bbc3baf6a32dbfb16cd8f5c81a60f70cecacac
Niyada <niyadanils@gmail.com>
2023-12-19 21:00:47 +0100

    fixes linting compliance

--- test/e2e/test_get_health.py
+++ test/e2e/test_get_health.py
@@ -1,4 +1,4 @@
-
+"""e2e test cases for testing the health endpoint of the application."""
 
 import requests
 import unittest
@@ -16,29 +16,44 @@ class TestE2EGetHealth(unittest.TestCase):
     
     def test_status_code(self):
         """Tests the status code of the health endpoint."""
-        response = requests.get(self.url, timeout=self.REQUEST_TIMEOUT)
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT
+        )
         self.assertEqual(response.status_code, 200)
     
     def test_content_type(self):
         """Tests the content type of the health endpoint."""
-        response = requests.get(self.url, timeout=self.REQUEST_TIMEOUT)
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT,
+        )
         self.assertEqual(response.headers["Content-Type"], "application/json")
 
     def test_res_body(self):
         """Tests the response body of the health endpoint."""
-        response = requests.get(self.url, timeout=self.REQUEST_TIMEOUT)
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT
+        )
         self.assertEqual(response.json(), {"healthy": True})
     
     def test_res_body_with_param(self):
         """Tests the response body of the health endpoint with a provided parameter."""
         message = "e2eTestMessage"
-        response = requests.get(f'{self.url}?message={message}', timeout=self.REQUEST_TIMEOUT)
+        response = requests.get(
+            f'{self.url}?message={message}', 
+            timeout=self.REQUEST_TIMEOUT,
+        )
         self.assertEqual(response.json(), {"healthy": True, "message": message})
     
     def test_res_body_with_unkown_param(self):
         """Tests the response body of the health endpoint with an unknown parameter."""
         message = "e2eTestMessage"
-        response = requests.get(f'{self.url}?unkownParameter={message}', timeout=self.REQUEST_TIMEOUT)
+        response = requests.get(
+            f'{self.url}?unkownParameter={message}',
+            timeout=self.REQUEST_TIMEOUT,
+        )
         expected_res_body = {"code": 400, "message": "Invalid parameter provided."} 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), expected_res_body)
+        self.assertEqual(response.json(), expected_res_body)    
