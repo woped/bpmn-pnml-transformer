cad392e7927fed4f3dd14324d6fc4f4d1b710493
Niyada <niyadanils@gmail.com>
2023-12-21 08:32:44 +0100

    Reworking tests (#13)
    * switched to unittest and implemented proper unit testing
    
    * Refactor test_get_health.py for improved readability and maintainability
    
    * Fix capitalization of test method name
    
    * Add __init__ module for testing
    
    * Add integration tests for health endpoint
    
    * Update integration workflow to include unit, integration, and e2e tests
    
    * fixes linting compliance
    
    * Add GCP authentication for integration tests
    
    * Add new dependencies to requirements.txt
    
    * Set E2E_URL environment variable in integration workflow
    
    * Add token_format and E2E_IDENTITY_TOKEN environment variable
    
    * now using access token instead of id token
    
    * trying id token with audience
    
    * debug message added for token
    
    * Update integration.yml to authenticate and set environment variables
    
    * Remove print statements from test_get_health.py
    
    * Refactor deployment and integration workflows

---
+++ test/e2e/test_get_health.py
@@ -0,0 +1,72 @@
+"""e2e test cases for testing the health endpoint of the application."""
+
+import requests
+import unittest
+
+import os
+
+class TestE2EGetHealth(unittest.TestCase):
+    """A e2e test class for testing the health endpoint of the application."""
+    
+    REQUEST_TIMEOUT = 50
+
+    def setUp(self):
+        """Performs setup before each test case."""
+        self.url = os.getenv("E2E_URL")
+        if self.url is None:
+            raise ValueError("E2E_URL environment variable not set.")
+        self.token = os.getenv("E2E_IDENTITY_TOKEN")
+        if self.token is None:
+            raise ValueError("E2E_IDENTITY_TOKEN environment variable not set.")
+        self.shared_haeaders = {
+            "Authorization": f"Bearer {self.token}"
+        }
+    
+    def test_status_code(self):
+        """Tests the status code of the health endpoint."""
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT,
+            headers=self.shared_haeaders,
+        )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_content_type(self):
+        """Tests the content type of the health endpoint."""
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT,
+            headers=self.shared_haeaders,
+        )
+        self.assertEqual(response.headers["Content-Type"], "application/json")
+
+    def test_res_body(self):
+        """Tests the response body of the health endpoint."""
+        response = requests.get(
+            self.url,
+            timeout=self.REQUEST_TIMEOUT,
+            headers=self.shared_haeaders,
+        )
+        self.assertEqual(response.json(), {"healthy": True})
+    
+    def test_res_body_with_param(self):
+        """Tests the response body of the health endpoint with a provided parameter."""
+        message = "e2eTestMessage"
+        response = requests.get(
+            f'{self.url}?message={message}', 
+            timeout=self.REQUEST_TIMEOUT,
+            headers=self.shared_haeaders,
+        )
+        self.assertEqual(response.json(), {"healthy": True, "message": message})
+    
+    def test_res_body_with_unkown_param(self):
+        """Tests the response body of the health endpoint with an unknown parameter."""
+        message = "e2eTestMessage"
+        response = requests.get(
+            f'{self.url}?unkownParameter={message}',
+            timeout=self.REQUEST_TIMEOUT,
+            headers=self.shared_haeaders,
+        )
+        expected_res_body = {"code": 400, "message": "Invalid parameter provided."} 
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), expected_res_body)    
