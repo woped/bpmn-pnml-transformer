f077331169508f9e87d8676d0746dd2c4f80602e
Niyada <niyadanils@gmail.com>
2024-05-13 19:30:08 +0200

    WWI21/29 configuring matrix ci pipeline (#83)
    * Integrate transformer
    
    * implements matrix build
    
    * fixes typo
    
    * adds second function for integration
    
    * fixes test setup for multiple functions
    
    * fixes module issue for tests
    
    * adds boilerplate tests for transform endpoint
    
    * testing dependecy caching
    
    * upes to build in caching functionality for pip
    
    * extracts python version into env var
    
    ---------
    
    Co-authored-by: Kevin <stolzkevin@hotmail.de>

---
+++ test/integration/transform/test_post_transform.py
@@ -0,0 +1,10 @@
+"""Integration tests for the transform endpoint of the application."""
+
+import unittest
+
+class TestUnitPostTransform(unittest.TestCase):
+    """An integration test class for testing the transform endpoint of the application."""
+
+    def test_boilerplate(self):
+        """Boilerplate empty test function. This will always pass."""
+        self.assertTrue(True)
