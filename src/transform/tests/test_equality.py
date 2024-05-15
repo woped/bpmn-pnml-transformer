4b338be76e838ff598be269d8cf6202f09c0d8e2
Benjamin Bleidorn <benjamin.bleidorn@sap.com>
2024-05-15 20:46:49 +0200

    Test equality and bpmn utility docu

--- src/transform/tests/test_equality.py
+++ src/transform/tests/test_equality.py
@@ -1,3 +1,7 @@
+"""Unit tests for the detection of subelements.
+
+Includes tests to check whether all PNML and BPMN subelemts are identified correctly.
+"""
 import unittest
 
 from transformer.equality.bpmn import get_all_processes_by_id
@@ -7,13 +11,16 @@ from transformer.models.pnml.pnml import Pnml
 
 
 class TestSubelements(unittest.TestCase):
+    """This class tests whether all subprocesses are identified."""
     def test_pnml_eqaulity_subprocess(self):
+        """Tests whether all pnml subprocesses are identified."""
         pnml = Pnml.from_file("tests/multiplesubprocesses.pnml")
         subnets = {}
         get_all_nets_by_id(pnml.net, subnets)
         self.assertEqual(len(subnets), 7)
 
     def test_bpmn_eqaulity_subprocess(self):
+        """Tests whether all bpmn subprocesses are identified."""
         bpmn = BPMN.from_file("tests/multiplesubprocesses.bpmn")
         subnets = {}
         get_all_processes_by_id(bpmn.process, subnets)
