96bf8378f653766cc737524ee659eedbc36439a6
Benjamin Bleidorn <benjamin.bleidorn@sap.com>
2024-05-20 14:45:59 +0200

    pnml base docu

--- src/transform/transformer/models/pnml/base.py
+++ src/transform/transformer/models/pnml/base.py
@@ -6,6 +6,7 @@ from transformer.utility.utility import WOPED, BaseModel
 
 
 class GenericNetIDNode(BaseModel):
+    """Generic Petri net node extension of BaseModel (+ID)."""
     id: str = attr()
 
     def __hash__(self):
@@ -13,22 +14,26 @@ class GenericNetIDNode(BaseModel):
 
 
 class Coordinates(BaseModel):
+    """Coordinate extension of BaseModel (+x and y)."""
     x: float = attr()
     y: float = attr()
 
 
 class Graphics(BaseModel, tag="graphics"):
+    """Graphics extension of BaseModel (+offset, dimension, position)."""
     offset: Coordinates | None = element("offset", default=None)
     dimension: Coordinates | None = element("dimension", default=None)
     position: Coordinates | None = element("position", default=None)
 
 
 class Name(BaseModel, tag="name"):
+    """Name extension of BaseModel (+graphics, title)."""
     graphics: Graphics | None = None
     title: str | None = element(tag="text", default=None)
 
 
 class Toolspecific(BaseModel, tag="toolspecific"):
+    """Toolspecific extension of BaseModel (+tool, version, transition,operator...)."""
     tool: str = attr(default=WOPED)
     version: str = attr(default="1.0")
 
@@ -51,50 +56,60 @@ class Toolspecific(BaseModel, tag="toolspecific"):
     subprocess: bool | None = element(tag="subprocess", default=None)
 
     def is_workflow_operator(self):
+        """Returns whether instance is a workflow operator."""
         if self.tool == WOPED and self.operator:
             return True
         return False
 
     def is_workflow_subprocess(self):
+        """Returns whether instance is a workflow subprocess."""
         if self.tool == WOPED and self.subprocess:
             return True
         return False
 
 
 class GenericNetNode(GenericNetIDNode):
+    """Generic NetNode extension of net id node."""
     pass
 
 
 class NetElement(GenericNetNode):
+    """Generic NetElement extension of GenericNetNode (+name, graphics, toolspecific)."""
     name: Name | None = None
     graphics: Graphics | None = None
     toolspecific: Toolspecific | None = None
 
     def get_name(self):
+        """Returns name of instance."""
         if not self.name:
             return None
         return self.name.title
 
     def __hash__(self):
+        """Return instance hashed."""
         return hash((type(self),) + (self.id,))
 
     def is_workflow_operator(self):
+        """Return whether instance is workflow operator."""
         if not self.toolspecific:
             return False
         return self.toolspecific.is_workflow_operator()
 
     def is_workflow_subprocess(self):
+        """Returns whether instance is workflow subprocess."""
         if not self.toolspecific:
             return False
         return self.toolspecific.is_workflow_subprocess()
 
     def mark_as_workflow_operator(self, type: WorkflowBranchingType, id: str):
+        """Mark this instance as workflow operator."""
         t = self
         if not t.toolspecific:
             t.toolspecific = Toolspecific()
         t.toolspecific.operator = Operator(id=id, type=type)
 
     def mark_as_workflow_subprocess(self):
+        """Mark this instance as a subprocess."""
         t = self
         if not t.toolspecific:
             t.toolspecific = Toolspecific()
@@ -103,5 +118,6 @@ class NetElement(GenericNetNode):
 
 
 class Inscription(BaseModel, tag="inscription"):
+    """Inscription extension of BaseModel (+text, graphics)."""
     text: str = element(tag="text")
     graphics: Graphics
