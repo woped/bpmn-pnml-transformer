edab84dccd95942bb14f10321c2ab9ec3ffb00de
Benjamin Bleidorn <benjamin.bleidorn@sap.com>
2024-05-21 15:41:04 +0200

    transform docu

--- src/transform/transformer/transform_petrinet_to_bpmn/transform.py
+++ src/transform/transformer/transform_petrinet_to_bpmn/transform.py
@@ -1,3 +1,4 @@
+"""Initiate the preprocessing and transformation of pnml to bpmn."""
 from collections.abc import Callable
 
 from transformer.models.bpmn.base import Gateway
@@ -23,6 +24,7 @@ from transformer.transform_petrinet_to_bpmn.workflow_helper import (
 
 
 def remove_silent_tasks(bpmn: Process):
+    """Remove silent tasks (Without name)."""
     for task in bpmn.tasks.copy():
         if task.name is not None:
             continue
@@ -31,6 +33,7 @@ def remove_silent_tasks(bpmn: Process):
 
 
 def remove_unnecessary_gateways(bpmn: Process):
+    """Remove unnecessary gateways (In and out degree <= 1)."""
     gw_nodes = {
         node for node in bpmn._flatten_node_typ_map() if issubclass(type(node), Gateway)
     }
@@ -42,6 +45,7 @@ def remove_unnecessary_gateways(bpmn: Process):
 
 
 def transform_petrinet_to_bpmn(net: Net):
+    """Initiate the transformation of a preprocessed petri net to bpmn."""
     bpmn_general = BPMN.generate_empty_bpmn(net.id or "new_net")
     bpmn = bpmn_general.process
 
@@ -109,6 +113,7 @@ def transform_petrinet_to_bpmn(net: Net):
 
 
 def apply_preprocessing(net: Net, funcs: list[Callable[[Net], None]]):
+    """Preprocess the whole petri net."""
     for p in net.pages:
         apply_preprocessing(p.net, funcs)
 
@@ -117,6 +122,7 @@ def apply_preprocessing(net: Net, funcs: list[Callable[[Net], None]]):
 
 
 def pnml_to_bpmn(pnml: Pnml):
+    """Process and transform a petri net to bpmn."""
     net = pnml.net
 
     apply_preprocessing(net, [split_different_operators])
