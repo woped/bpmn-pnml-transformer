9866a11861da63a343cff26fb070caaf9a16f258
Kevin <stolzkevin@hotmail.de>
2024-05-30 08:33:10 +0200

    Update Docs misc

--- src/transform/transformer/utility/utility.py
+++ src/transform/transformer/utility/utility.py
@@ -9,17 +9,17 @@ WOPED = "WoPeD"
 
 
 def get_tag_name(element: Element):
-    """Return the name of an element."""
+    """Return the lowercase tag name of an element."""
     return etree.QName(element).localname.lower()
 
 
 def create_silent_node_name(source: str, target: str):
-    """Return a silent node (name) from source to target."""
+    """Construct a silent node (name) from source to target."""
     return f"SILENTFROM{source}TO{target}"
 
 
 def create_arc_name(source: str | None, target: str | None):
-    """Return an arc (name) from source to target."""
+    """Construct an arc (name) from source to target."""
     if source is None or target is None:
         raise Exception("source and target must have a value")
     return f"{source}TO{target}"
@@ -41,6 +41,6 @@ class BaseBPMNModel(
     skip_empty=True,
     nsmap={"": "http://www.omg.org/spec/BPMN/20100524/MODEL"},
 ):
-    """BaseModel extension of BaseXmlModel."""
+    """BaseBPMNModel extension of BaseXmlModel."""
 
     pass
