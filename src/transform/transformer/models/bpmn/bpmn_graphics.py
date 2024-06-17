ebf578cc66439e838117d746a793eeeb73e0fc34
Benjamin Bleidorn <benjamin.bleidorn@sap.com>
2024-06-17 17:30:42 +0200

    Handle BPMNLabel on Object level, planes can now also have a label

--- src/transform/transformer/models/bpmn/bpmn_graphics.py
+++ src/transform/transformer/models/bpmn/bpmn_graphics.py
@@ -6,17 +6,6 @@ from transformer.models.bpmn.base import ns_map
 from transformer.utility.utility import BaseBPMNModel
 
 
-class BPMNDINamespace(BaseBPMNModel, ns="bpmndi", nsmap=ns_map):
-    """BPMNDINNamespace extension of BaseBPMNModel."""
-
-    pass
-
-
-class BPMNDIObject(BPMNDINamespace):
-    """BPMNDIObject extension of BPMNDINamespace with element xml attribute."""
-
-    bpmnElement: str = attr()
-
 
 class DCBounds(BaseBPMNModel, tag="Bounds", ns="dc", nsmap=ns_map):
     """DCBounds extension of BaseBPMNModel with x,y, width and height xml attributes."""
@@ -27,6 +16,24 @@ class DCBounds(BaseBPMNModel, tag="Bounds", ns="dc", nsmap=ns_map):
     height: float = attr()
 
 
+class BPMNDINamespace(BaseBPMNModel, ns="bpmndi", nsmap=ns_map):
+    """BPMNDINNamespace extension of BaseBPMNModel."""
+
+    pass
+
+class BPMNLabel(BPMNDINamespace, tag="BPMNLabel"):
+    """BPMNLabel extension of BPMNIDNamespace with DCBounds attribute."""
+
+    bounds: DCBounds | None = element(default=None)
+
+class BPMNDIObject(BPMNDINamespace):
+    """BPMNDIObject extension of BPMNDINamespace with element xml attribute."""
+
+    bpmnElement: str = attr()
+    label: BPMNLabel | None = element(default=None)
+
+
+
 class DIWaypoint(BaseBPMNModel, tag="waypoint", ns="di", nsmap=ns_map):
     """DIWaypoint extension of BaseBPMNModel with x and y xml attribute."""
 
@@ -34,17 +41,12 @@ class DIWaypoint(BaseBPMNModel, tag="waypoint", ns="di", nsmap=ns_map):
     y: float = attr(default=0)
 
 
-class BPMNLabel(BPMNDINamespace, tag="BPMNLabel"):
-    """BPMNLabel extension of BPMNIDNamespace with DCBounds attribute."""
-
-    bounds: DCBounds | None = element(default=None)
 
 
 class BPMNEdge(BPMNDIObject, tag="BPMNEdge"):
     """BPMNEdge extension of BPMNDIObject with waypoints and label."""
 
     waypoints: list[DIWaypoint] = element(default_factory=list)
-    label: BPMNLabel | None = element(default=None)
 
 
 class BPMNShape(BPMNDIObject, tag="BPMNShape"):
@@ -52,7 +54,6 @@ class BPMNShape(BPMNDIObject, tag="BPMNShape"):
 
     bounds: DCBounds
     isExpanded: bool | None = attr(default=None)
-    label: BPMNLabel | None = element(default=None)
 
 
 class BPMNPlane(BPMNDIObject, tag="BPMNPlane"):
