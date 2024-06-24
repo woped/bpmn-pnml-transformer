8eb2b413bb22c624462bba08e92de2f62407b80e
Jeldrik Merkelbach <jeldrik.merkelbach@sap.com>
2024-06-24 11:29:37 +0200

    improved code quality

--- src/checkTokens/main.py
+++ src/checkTokens/main.py
@@ -2,12 +2,10 @@
 
 This module defines a Google Cloud Function for RateLimiting the transform Endpoint.
 """
-import tempfile
 import base64
 import firebase_admin
 import functions_framework
 import json
-import re
 from firebase_admin import credentials, firestore
 from flask import jsonify
 import os
@@ -17,42 +15,21 @@ GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 = os.getenv( "GCP_SERVICE_ACCOUNT_CERTIFI
 if( GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 is None ):
     print( "Env var GCP_SERVICE_ACCOUNT_CERTIFICATE not found!" )
 
-print('Base64:' + GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64)
-
 GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_BYTES = \
     base64.b64decode(GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64)
 
 GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING = \
     GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_BYTES.decode('utf-8')
 
-print('Decoded String: ' + GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING)
+cred_dict = json.loads(GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING, strict=False)
 
-cleaned_string = re.sub(r'[^\x20-\x7E]', ''
-                        , GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING)
+#with tempfile.NamedTemporaryFile(delete=False, mode='w') as temp_file:
+ #   json.dump(json_data, temp_file, ensure_ascii=False)
+  #  temp_file_path = temp_file.name
 
-cleaned_string = cleaned_string.replace('\n', '').strip()
-print('Cleaned String:  ' + cleaned_string)
-json_data = json.loads(GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING, strict=False)
-
-
-with tempfile.NamedTemporaryFile(delete=False, mode='w') as temp_file:
-    #temp_file.write(GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING.encode('utf-8'))
-    #temp_file.write(cleaned_string.encode('utf-8'))
-    json.dump(json_data, temp_file, ensure_ascii=False)
-    temp_file_path = temp_file.name
-
-with open(temp_file_path) as file:
-    content = file.read()
-
-print('content:' + content)
-
-print(temp_file_path)
-print('test')
-
-cred = credentials.Certificate(temp_file_path)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
 
 @functions_framework.http
 def check_tokens(request):
