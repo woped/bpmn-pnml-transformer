e6293f2e892e89f1ef156de5a01e4db67033507f
Jeldrik Merkelbach <76696085+Jmp-7@users.noreply.github.com>
2024-06-24 15:02:51 +0200

    Wwi21/46 implementing rate limiting (#172)
    * Basic logic for token checking
    
    * Initial Firebase Setup
    
    * Implemented token system
    
    * Linting checks
    
    * Linting checks
    
    * firebase_admin dependency added
    
    * added requirement
    
    * firebase_admin added to specific requirements
    
    * adds service account certificate env var from github secrests
    
    * removes superflous dependency 'pnmlbpmntransformer'
    
    * :green_heart:
    
    * :green_heart: Changes service account certificate env var handling
    
    * Check Tokens Function added
    
    * adds basic decodation of base64 service account certificate
    
    * Refactor service account certificate handling
    
    * fixes set_force_std_xml flag position
    
    * now uses correct var for decoding base64
    
    * added tempfile for secret
    
    * remove unused dependency
    
    * test prints added
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * t
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * improved code quality
    
    * final touches
    
    * fixes cd pipeline: sets necessary env var for gcp service account certificate
    
    * fix cd pipeline: also sets env var in matrix job
    
    * replaces print message for missing env var with exception
    
    * swaps checkout and env set step in cd pipeline
    
    * removes workflow dispatch and finally fixes CD env var (hopefully)
    
    * Initial Setup for refreshTokens endpoint
    
    * fixed app.py
    
    * fix method name
    
    ---------
    
    Co-authored-by: Jeldrik Merkelbach <jeldrik.merkelbach@sap.com>
    Co-authored-by: Niyada <niyadanils@gmail.com>

---
+++ src/refreshTokens/main.py
@@ -0,0 +1,40 @@
+"""Implements the 'refresh_Tokens' HTTP Cloud Function.
+
+This module defines a Google Cloud Function for Refreshing Tokens of the .
+"""
+import base64
+import firebase_admin
+import functions_framework
+import json
+from firebase_admin import credentials, firestore
+from flask import jsonify
+import os
+
+GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 = os.getenv( "GCP_SERVICE_ACCOUNT_CERTIFICATE" )
+if( GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 is None ):
+    raise KeyError( "Env var GCP_SERVICE_ACCOUNT_CERTIFICATE not found!" )
+
+GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_BYTES = \
+    base64.b64decode(GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64)
+
+GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING = \
+    GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_BYTES.decode('utf-8')
+
+cred_dict = json.loads(GCP_SERVICE_ACCOUNT_CERTIFICATE_DECODED_STRING, strict=False)
+cred = credentials.Certificate(cred_dict)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+@functions_framework.http
+def refresh_tokens(request):
+    """Refresh the available tokens in the Firestore database."""
+    if db is None:
+        return jsonify({"error": "No database available"}), 500
+    doc_ref = db.collection("api-tokens").document("token-document")
+    doc = doc_ref.get()
+    if doc.exists:
+        doc_ref.update({"tokens": 100})
+        return jsonify({"Tokens set to 100"}), 200
+    else:
+        return jsonify({"error": "No document available"}), 404
+    
