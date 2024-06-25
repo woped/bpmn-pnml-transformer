f38e640fbf6b78ed9d2bc60d54c86713b4d4ca54
Jeldrik Merkelbach <76696085+Jmp-7@users.noreply.github.com>
2024-06-25 14:57:08 +0200

    Wwi21/46 implementing rate limiting (#176)
    * Basic logic for token checking
    
    * Initial Firebase Setup
    
    * Implemented token system
    
    * Linting checks
    
    * Linting checks
    
    * firebase_admin dependency added
    
    * added requirement
    
    * firebase_admin added to specific requirements
    
    * adds service account certificate env var from github secrests
    
    * removes superflous dependency 'pnmlbpmntransformer'
    
    * :green_heart:
    
    * :green_heart: Changes service account certificate env var handling
    
    * Check Tokens Function added
    
    * adds basic decodation of base64 service account certificate
    
    * Refactor service account certificate handling
    
    * fixes set_force_std_xml flag position
    
    * now uses correct var for decoding base64
    
    * added tempfile for secret
    
    * remove unused dependency
    
    * test prints added
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * t
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * improved code quality
    
    * final touches
    
    * fixes cd pipeline: sets necessary env var for gcp service account certificate
    
    * fix cd pipeline: also sets env var in matrix job
    
    * replaces print message for missing env var with exception
    
    * swaps checkout and env set step in cd pipeline
    
    * removes workflow dispatch and finally fixes CD env var (hopefully)
    
    * Initial Setup for refreshTokens endpoint
    
    * fixed app.py
    
    * fix method name
    
    * added matrix build for deployment of refresh tokens
    
    * Fixed jsonify error
    
    * adjusted check_token with timestamp logic
    
    * added pytz requirement
    
    ---------
    
    Co-authored-by: Jeldrik Merkelbach <jeldrik.merkelbach@sap.com>
    Co-authored-by: Niyada <niyadanils@gmail.com>

--- src/checkTokens/main.py
+++ src/checkTokens/main.py
@@ -9,6 +9,8 @@ import json
 from firebase_admin import credentials, firestore
 from flask import jsonify
 import os
+import pytz
+from datetime import datetime, timedelta
 
 GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 = os.getenv( "GCP_SERVICE_ACCOUNT_CERTIFICATE" )
 if( GCP_SERVICE_ACCOUNT_CERTIFICATE_BASE64 is None ):
@@ -36,8 +38,22 @@ def check_tokens(request):
     doc = doc_ref.get()
     if doc.exists:
         tokens = doc.to_dict().get("tokens", 0)
+        timestamp = doc.to_dict().get("tokens_last_replenished", 0)
+        current_time = datetime.now(pytz.utc)
+        time_difference = current_time - timestamp
+
         if tokens <= 0:
-            return jsonify({"error": "No tokens available"}), 400
+            if time_difference >= timedelta(hours=1):
+                doc_ref.update({
+                    "tokens": 99,
+                    "tokens_last_replenished": current_time
+                })
+                return jsonify({"tokens": 99})
+            else:
+                return jsonify({"error":
+                                 "No tokens available, and last replenish" +
+                                 "was less than an hour ago." + 
+                                 "Please try again later."}), 400
         else:
             doc_ref.update({"tokens": tokens-1})
             return jsonify({"tokens": tokens-1}), 200
