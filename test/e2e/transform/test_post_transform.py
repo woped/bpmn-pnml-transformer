924d4e9dfac245121f96eb233d10595f9aa04a4b
nils.baumgartner <nils.baumgartner@sap.com>
2024-06-28 15:21:16 +0200

    e2e tests

--- test/e2e/transform/test_post_transform.py
+++ test/e2e/transform/test_post_transform.py
@@ -22,69 +22,18 @@ class TestE2EPostTransform(unittest.TestCase):
         }
 
     def test_pnml_to_bpmn(self):
-        """Tests the status code of the transform endpoint."""
+        """Tests the transform endpoint for direction pnmltobpmn."""
+        bpmn_xml = '../diagrams/xml/e2e_bpmn_res.xml'
+        with open(bpmn_xml, encoding='utf-8') as file:
+            bpmn_string = file.read()
+        pnml_xml = '../diagrams/xml/e2e_bpmn_res.xml'
+        with open(pnml_xml, encoding='utf-8') as file:
+            pnml_string = file.read()
         expected_response = {
-            "bpmn": (
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                "<bpmn:definitions xmlns:bpmn=\"http://www.omg.org/spec/BPMN/20100524"
-                "/MODEL\" "
-                "xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" "
-                "xmlns:dc=\"http://www.omg.org/spec/DD/20100524/DC\" "
-                "xmlns:di=\"http://www.omg.org/spec/DD/20100524/DI\">"
-                "<bpmn:process id=\"Process_05gf0wk\" isExecutable=\"true\">"
-                "<bpmn:startEvent id=\"StartEvent_1kldrri\">"
-                "<bpmn:outgoing>StartEvent_1kldrriTOActivity_16g2nsl</bpmn:outgoing>"
-                "</bpmn:startEvent>"
-                "<bpmn:endEvent id=\"Event_02tt0ub\">"
-                "<bpmn:incoming>Activity_16g2nslTOEvent_02tt0ub</bpmn:incoming>"
-                "</bpmn:endEvent>"
-                "<bpmn:task id=\"Activity_16g2nsl\" name=\"Task\">"
-                "<bpmn:incoming>StartEvent_1kldrriTOActivity_16g2nsl</bpmn:incoming>"
-                "<bpmn:outgoing>Activity_16g2nslTOEvent_02tt0ub</bpmn:outgoing>"
-                "</bpmn:task>"
-                "<bpmn:sequenceFlow id=\"StartEvent_1kldrriTOActivity_16g2nsl\" source"
-                "Ref=\"StartEvent_1kldrri\" targetRef=\"Activity_16g2nsl\" />"
-                "<bpmn:sequenceFlow id=\"Activity_16g2nslTOEvent_02tt0ub\" source"
-                "Ref=\"Activity_16g2nsl\" targetRef=\"Event_02tt0ub\" />"
-                "</bpmn:process>"
-                "<bpmndi:BPMNDiagram id=\"diagram1\">"
-                "<bpmndi:BPMNPlane id=\"planeProcess_05gf0wk\" bpmnElement=\"Process_0"
-                "5gf0wk\">"
-                "<bpmndi:BPMNEdge id=\"StartEvent_1kldrriTOActivity_16g2nsl_di\" bpmn"
-                "Element=\"StartEvent_1kldrriTOActivity_16g2nsl\">"
-                "<di:waypoint x=\"0.0\" y=\"0.0\" /><di:waypoint x=\"0.0\" y=\"0.0\" />"
-                "</bpmndi:BPMNEdge>"
-                "<bpmndi:BPMNEdge id=\"Activity_16g2nslTOEvent_02tt0ub_di\" bpmn"
-                "Element=\"Activity_16g2nslTOEvent_02tt0ub\">"
-                "<di:waypoint x=\"0.0\" y=\"0.0\" /><di:waypoint x=\"0.0\" y=\"0.0\" />"
-                "</bpmndi:BPMNEdge>"
-                "<bpmndi:BPMNShape id=\"Activity_16g2nsl_di\" bpmn"
-                "Element=\"Activity_16g2nsl\">"
-                "<dc:Bounds x=\"0.0\" y=\"0.0\" width=\"100.0\" height=\"80.0\" />"
-                "</bpmndi:BPMNShape>"
-                "<bpmndi:BPMNShape id=\"StartEvent_1kldrri_di\" bpmnElement=\"Start"
-                "Event_1kldrri\">"
-                "<dc:Bounds x=\"0.0\" y=\"0.0\" width=\"100.0\" height=\"80.0\" />"
-                "</bpmndi:BPMNShape>"
-                "<bpmndi:BPMNShape id=\"Event_02tt0ub_di\" bpmnElement=\"Event_02tt0ub\""
-                "><dc:Bounds x=\"0.0\" y=\"0.0\" width=\"100.0\" height=\"80.0\" />"
-                "</bpmndi:BPMNShape>"
-                "</bpmndi:BPMNPlane>"
-                "</bpmndi:BPMNDiagram>"
-                "</bpmn:definitions>"
-            )
+            "bpmn": bpmn_string
         }
         payload = {
-            "pnml": (
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><pnml><net id=\"Process_0"
-                "5gf0wk\"><place id=\"StartEvent_1kldrri\" /><place id=\"Event_02t"
-                "t0ub\" /><transition id=\"Activity_16g2nsl\"><name><graphics><offset x"
-                "=\"20.0\" y=\"20.0\" /></graphics><text>Task</text></name></transition>"
-                "<arc id=\"Activity_16g2nslTOEvent_02tt0ub\" source=\"Activity_16g2nsl\""
-                " target=\"Event_02tt0ub\" /><arc id=\"StartEvent_1kldrriTOActivity_16"
-                "g2nsl\" source=\"StartEvent_1kldrri\" target=\"Activity_16g2nsl\" />"
-                "</net></pnml>"
-            )
+            "pnml": pnml_string
         }
         response = requests.post(
             f'{self.url}?direction=pnmltobpmn', 
@@ -97,7 +46,7 @@ class TestE2EPostTransform(unittest.TestCase):
         self.assertEqual(response.json(), expected_response)
 
     def test_bpmn_to_pnml(self):
-        """Tests the status code of the transform endpoint."""
+        """Tests the transform endpoint for direction bpmntopnml."""
         expected_response = {
             "pnml": (
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?><pnml id=\"\"><net id=\"Proce"
